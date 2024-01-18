4d7320f2d956fa90a015a91178bf09cf9e56e511
MChowdhury247 <mtichowdhury247@gmail.com>
2024-01-18 09:42:38 -0500

    learned a few other things

--- hungry.py
+++ hungry.py
@@ -1,5 +1,14 @@
-hungry = input("Are you Hungry?")
-if hungry=="Yes":
+""" hungry = input("Are you Hungry?")
+if hungry=="yes":
     print("Eat IceCream")
 else:
-    print("do your homework")
+    print("do your homework") """
+
+'''x = 3
+print(complex(x))'''
+
+
+a= """My name is Muid, "SO what", she said
+"""
+
+print(a)
