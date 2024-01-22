7765edcb9eded444552ac5a97ba63b3526e3e98f
MChowdhury247 <mtichowdhury247@gmail.com>
2024-01-22 10:56:26 -0500

    just the beginning

--- Project1.py
+++ Project1.py
@@ -1,1 +1,23 @@
-print("hello")
+import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+# value = roll()
+# print(value)
+
+while True:
+    players = input("Enter the number of players (2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            print("It should be between 2 - 4 players")
+            break
+        else:
+            print("Invalid number, please try again")
+
+print(players)
